b060a5358cb127023abf0a869510f08a1a9b8392
AndreyF <sever-katichka@rambler.ru>
2022-12-11 20:00:22 +0500

    add_dbName_and_increase

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -6,6 +6,13 @@ import EmployersAddForm from '../employers-add-form/employers-add-form';
 import './app.css';
 
 function App() {
+
+    const data = [
+        {name: 'Алексей Семизаров', salary: 38500, increase: false},
+        {name: 'Константин Булашов', salary: 43800, increase: false},
+        {name: 'Андрей Федоренко', salary: 59600, increase: true}
+    ]
+
     return (
         <div className="app">
             <AppInfo />
@@ -13,7 +20,7 @@ function App() {
                 <SearchPanel/>
                 <AppFilter/>
             </div>
-            <EmployersList />
+            <EmployersList data={data}/>
             <EmployersAddForm/>
         </div>
 
