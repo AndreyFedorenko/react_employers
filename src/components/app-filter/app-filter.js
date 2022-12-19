1a13e13792b89912b9ce140c64e2e7915bef6e8e
AndreyF <sever-katichka@rambler.ru>
2022-12-20 00:19:07 +0500

    filter_and_search

--- src/components/app-filter/app-filter.js
+++ src/components/app-filter/app-filter.js
@@ -1,25 +1,33 @@
 import './app-filter.css';
 
-const AppFilter = () => {
-    return (
-            <button className="btn-group">
-                <button 
-                    type="button"
-                    className="btn btn-light">
-                    Все сотрудники
-                </button>
-                <button 
-                    type="button"
-                    className="btn btn-outline-light">
-                    На повышение
-                </button>
-                <button
-                    type="button" 
-                    className="btn btn-outline-light">
-                    З/П больше 45000р.
-                </button>
+const AppFilter = (props) => {
+
+    const buttonsData = [
+        {name: 'all', label: 'Все сотрудники'},
+        {name: 'all-rise', label: 'На повышение'},
+        {name: 'all-salary', label: 'З\П более 45000'},
+    ];
+    const buttons = buttonsData.map(({name, label}) => {
+        const active = props.filter === name;
+        const clazz = active ? 'btn-light' : 'btn-outline-light';
+        return (
+            <button 
+                type="button"
+                className={`btn ${clazz}`}
+                key={name}
+                onClick={() => props.onFilterSelect(name)}
+                >
+                {label}
             </button>
+        )
+    })
+
+    return (
+        <div className="btn-group">
+            {buttons}
+        </div>
     )
+
 }
 
 export default AppFilter;
--- src/components/employers-add-form/employers-add-form.js
+++ src/components/employers-add-form/employers-add-form.js
@@ -1,23 +1,58 @@
+import { Component } from 'react';
 import './employers-add-form.css';
 
-const EmployersAddForm = () => {
-    return (
-        <div className="app-add-form">
-        <h3>Добавьте нового сотрудника</h3>
-        <form
-            className="add-form d-flex">
-            <input type="text"
-                className="form-control new-post-label"
-                placeholder="Как его зовут?" />
-            <input type="number"
-                className="form-control new-post-label"
-                placeholder="З/П в руб?" />
+class EmployersAddForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            salary: ''
+        }
+    }
 
-            <button type="submit"
-                    className="btn btn-outline-light">Добавить</button>
-        </form>
-    </div>
-    )
+    onValueChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    onSubmit = (e) => {
+        e.preventDefault();
+        this.props.onAdd(this.state.name, this.state.salary);
+        this.setState({
+            name: '',
+            salary: ''
+        })
+    }
+
+    render() {
+        const {name, salary} = this.state
+
+        return (
+            <div className="app-add-form">
+            <h3>Добавьте нового сотрудника</h3>
+            <form
+                className="add-form d-flex"
+                onSubmit={this.onSubmit}>
+                <input type="text"
+                    className="form-control new-post-label"
+                    placeholder="Укажите имя и фамилию нового сотрудника"
+                    name="name"
+                    value={name} 
+                    onChange={this.onValueChange} required/>
+                <input type="number"
+                    className="form-control new-post-label"
+                    placeholder="Укажите заработную плату в рублях"
+                    name="salary"
+                    value={salary} 
+                    onChange={this.onValueChange} required/>
+    
+                <button type="submit"
+                        className="btn btn-outline-light">Добавить</button>
+            </form>
+        </div>
+        )
+    }
 }
 
 export default EmployersAddForm;
